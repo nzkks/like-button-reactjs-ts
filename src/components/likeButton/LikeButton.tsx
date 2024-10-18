7932d5da39b2fe67fd264abc8845557539206a17
nzkks <kkshanthosh@gmail.com>
2024-10-18 16:34:33 +1300

    Integrate the Like button click action with an api

--- src/components/likeButton/LikeButton.tsx
+++ src/components/likeButton/LikeButton.tsx
@@ -4,8 +4,29 @@ import { HeartIcon } from '../icons';
 
 const LikeButton = () => {
   const [Liked, setLiked] = useState(false);
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState(null);
 
-  const handleClick = () => {
+  const handleClick = async () => {
+    setIsFetching(true);
+    setError(null);
+
+    try {
+      const response = await fetch('https://www.greatfrontend.com/api/questions/like-button', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          action: Liked ? 'unlike' : 'like',
+        }),
+      });
+
+      console.log(await response.json());
+    } finally {
+      setIsFetching(false);
+    }
+
     setLiked(!Liked);
   };
 
